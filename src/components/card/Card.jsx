3eec1e82c1ed1ac49c0e8f2e43c2e6a7a8cdc272
AshleyMiller79 <ipekbilir79@gmail.com>
2022-03-21 23:36:07 +0300

    grg

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,7 +4,27 @@ import "./Card.scss"
 const Card = ({veri}) => {
  
   return (
-    <div>
+    <div className="container">
+ 
+ { veri.map((eleman)=>{
+  //destructuring
+  const {id, countries, capital, img,population} = eleman
+  return(
+
+<div className="card" key={id}    >
+  
+ <h1>{ countries }</h1>
+      <img src={img }/>
+<h2> {capital}</h2>
+<h4>{population}</h4>
+  
+  
+      </div>
+
+
+  )
+ })    }
+
       
     </div>
   )
